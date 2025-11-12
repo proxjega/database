e2d7b23dfbf029285229e9b795205245481f1018
Tomas178 <tomaspetronis77@gmail.com>
2025-11-12 15:49:34 +0200

    Pataisiau pagal .clang-tidy tiek, kad kodas vis dar veiktu

--- btree/include/leafpage.h
+++ btree/include/leafpage.h
@@ -5,15 +5,15 @@
 class LeafPage : public BasicPage {
        friend class Database;
     public:
-        
+
         // Constructors
         using BasicPage::BasicPage;
-        LeafPage(uint32_t ID);
+        LeafPage(uint32_t pageID);
 
         // Helpers
         uint16_t FindInsertPosition(const string& key);
         int16_t FindKeyIndex(const string& key);
-        bool WillFit(string key, string value);
+        bool WillFit(const string &key, const string &value);
         LeafPage Optimize();
 
         // Operations
@@ -21,7 +21,7 @@ class LeafPage : public BasicPage {
         leafNodeCell GetKeyValue(uint16_t offset);
         std::optional<leafNodeCell> FindKey(const string &key);
         void RemoveKey(const string &key);
-        
+
         // For debug
         void CoutPage();
 };
