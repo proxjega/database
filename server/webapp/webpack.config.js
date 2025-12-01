f527077938607527f080ae7193a89fa4e477f2e6
MatasSioma <matassemenas@gmail.com>
2025-12-01 02:01:09 +0200

    npm build fix for git

--- server/webapp/webpack.config.js
+++ server/webapp/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, '../public'),
-    filename: 'js/app.[contenthash].js',
+    filename: 'js/app.js',
     publicPath: '/',
     clean: true
   },
